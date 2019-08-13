ee4572303faf8e753464469056283b226a7e6dde
Aleksey Kapustyanenko <lehkap@hotmail.com>
2019-08-13 22:28:21 +0400

    Add LinqKit demo

--- example/Ef6Part9000/LiveDemo2/Program.cs
+++ example/Ef6Part9000/LiveDemo2/Program.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Data.Entity.Infrastructure;
 using System.Diagnostics;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -24,7 +25,7 @@ namespace LiveDemo2
 
             while (true)
             {
-                Console.Write("Выбирете пример 1 - плохой, 2 - хороший: ");
+                Console.Write("Выбирете пример 1 - плохой, 2 - NotGood, 3 - NotBad 4 - хороший,: ");
                 var number = Console.ReadLine();
                 switch (number)
                 {
@@ -38,6 +39,24 @@ namespace LiveDemo2
                             break;
                         }
                     case "2":
+                        {
+                            var stopwatch = Stopwatch.StartNew();
+                            var sql = NotGoodQuery();
+                            stopwatch.Stop();
+                            Clipboard.SetText(sql);
+                            Console.WriteLine($"Хороший запрос выполнился за {stopwatch.ElapsedMilliseconds}");
+                            break;
+                        }
+                    case "3":
+                        {
+                            var stopwatch = Stopwatch.StartNew();
+                            var sql = NotBadQuery();
+                            stopwatch.Stop();
+                            Clipboard.SetText(sql);
+                            Console.WriteLine($"Хороший запрос выполнился за {stopwatch.ElapsedMilliseconds}");
+                            break;
+                        }
+                    case "4":
                         {
                             var stopwatch = Stopwatch.StartNew();
                             var sql = GoodQuery();
@@ -57,6 +76,43 @@ namespace LiveDemo2
             using (var context = new BookStoreContext())
             {
                 var query = context.Orders.Where(o => statusValues.Contains(o.Status));
+                var result = query.ToList();
+                return ((DbQuery<Order>)query).Sql;
+            }
+        }
+
+        static string NotGoodQuery()
+        {
+            var statusValues = new StatusEnum[] { StatusEnum.New, StatusEnum.Processed };
+            using (var context = new BookStoreContext())
+            {
+                var query = context.Orders.Where(o => statusValues[0]==o.Status || statusValues[2]==o.Status);
+                var result = query.ToList();
+                return ((DbQuery<Order>)query).Sql;
+            }
+        }
+
+        static string NotBadQuery()
+        {
+            var statusValues = new StatusEnum[] { StatusEnum.New, StatusEnum.Processed };
+            var expressionParameter = Expression.Parameter(typeof(Order), "o");
+            var expressionMember = Expression.PropertyOrField(expressionParameter, "Status");
+            Expression bodyExpr= Expression.Constant(false, typeof(bool));
+            int n = 0;
+            foreach(var status in statusValues)
+            {
+                var exprConst = Expression.Constant(status, typeof(StatusEnum));
+                
+                var exprVar=Expression.Property(Expression.Constant(new { Value = status }), $"Value");
+                var expEq = Expression.Equal(expressionMember, exprVar);
+                bodyExpr = Expression.Or(bodyExpr, expEq);
+                n++;
+            }
+            var expressionLambda = Expression.Lambda<Func<Order, bool>>(bodyExpr, expressionParameter);
+
+            using (var context = new BookStoreContext())
+            {
+                var query = context.Orders.Where(expressionLambda);
                 var result = query.ToList();
                 return ((DbQuery<Order>)query).Sql;
             }
